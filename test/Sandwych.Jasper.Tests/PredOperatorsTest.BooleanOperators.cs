ca80deca5dcb8a3c05f40ddc7dfca2ee7292b78e
Li Wei <oldre@gmail.com>
2022-11-07 13:51:31 +0800

    Add more test

---
+++ test/Sandwych.Jasper.Tests/PredOperatorsTest.BooleanOperators.cs
@@ -0,0 +1,68 @@
+using Sandwych.Jasper.Tests.Models;
+
+namespace Sandwych.Jasper.Tests;
+
+public partial class PredOperatorsTest {
+
+    [Fact]
+    public void AndOperatorTest1() {
+        var pairs = new KeyValuePair<string, decimal>[] {
+            new KeyValuePair<string, decimal>("a", 1),
+            new KeyValuePair<string, decimal>("b", 2),
+            new KeyValuePair<string, decimal>("c", 3),
+        };
+
+        var json = @"
+[
+    ""and"", 
+    [""="", ""Key"", ""b""],
+    [""="", ""Value"", 2]
+]
+";
+        var filteredByJson = pairs.AsQueryable().WhereByJson(json).ToArray();
+        var filteredByLinq = pairs.Where(x => x.Key == "b" && x.Value == 2).ToArray();
+        Assert.Equal(filteredByLinq, filteredByJson);
+    }
+
+    [Fact]
+    public void OrOperatorTest1() {
+        var pairs = new KeyValuePair<string, decimal>[] {
+            new KeyValuePair<string, decimal>("a", 1),
+            new KeyValuePair<string, decimal>("b", 2),
+            new KeyValuePair<string, decimal>("c", 3),
+        };
+
+        var json = @"
+[
+    ""or"", 
+    [""="", ""Key"", ""b""],
+    [""="", ""Value"", 1]
+]
+";
+        var filteredByJson = pairs.AsQueryable().WhereByJson(json).ToArray();
+        var filteredByLinq = pairs.Where(x => x.Key == "b" || x.Value == 1).ToArray();
+        Assert.Equal(filteredByLinq, filteredByJson);
+    }
+
+    [Fact]
+    public void NotOperatorTest1() {
+        var pairs = new KeyValuePair<string, decimal>[] {
+            new KeyValuePair<string, decimal>("a", 1),
+            new KeyValuePair<string, decimal>("b", 2),
+            new KeyValuePair<string, decimal>("c", 3),
+        };
+
+        var json = @"
+[
+    ""and"", 
+    [""="", ""Key"", ""b""],
+    [""not"", [""="", ""Value"", 1]]
+]
+";
+        var filteredByJson = pairs.AsQueryable().WhereByJson(json).ToArray();
+        var filteredByLinq = pairs.Where(x => x.Key == "b" && !(x.Value == 1)).ToArray();
+        Assert.Equal(filteredByLinq, filteredByJson);
+    }
+
+
+}
--- test/Sandwych.Jasper.Tests/PredOperatorsTest.ComparisonOperators.cs
+++ test/Sandwych.Jasper.Tests/PredOperatorsTest.ComparisonOperators.cs
@@ -5,36 +5,31 @@ namespace Sandwych.Jasper.Tests;
 public partial class PredOperatorsTest {
 
     [Fact]
-    public void InOperatorTest1() {
+    public void LessThanOperatorTest() {
         var pairs = new KeyValuePair<string, decimal>[] {
             new KeyValuePair<string, decimal>("a", 1),
             new KeyValuePair<string, decimal>("b", 2),
             new KeyValuePair<string, decimal>("c", 3),
         };
 
-        var json = @"
-[
-    ""and"", 
-    [""in"", ""Value"", [1, 2]], 
-    [""="", ""Key"", ""b""]
-]
-";
+        var json = @"[ ""<"", ""Value"", 2 ]";
         var filteredByJson = pairs.AsQueryable().WhereByJson(json).ToArray();
-        var filteredByLinq = pairs.Where(x => (x.Value == 1 || x.Value == 2) && x.Key == "b").ToArray();
+        var filteredByLinq = pairs.Where(x => x.Value < 2).ToArray();
         Assert.Equal(filteredByLinq, filteredByJson);
     }
 
     [Fact]
-    public void InOperatorTest2() {
+    public void LessThanOrEqualOperatorTest() {
         var pairs = new KeyValuePair<string, decimal>[] {
             new KeyValuePair<string, decimal>("a", 1),
             new KeyValuePair<string, decimal>("b", 2),
             new KeyValuePair<string, decimal>("c", 3),
         };
 
-        var json = "[\"in\", \"Value\", [1, 2]]";
+        var json = @"[""<="", ""Value"", 2]";
         var filteredByJson = pairs.AsQueryable().WhereByJson(json).ToArray();
-        var filteredByLinq = pairs.Where(x => (x.Value == 1 || x.Value == 2)).ToArray();
+        var filteredByLinq = pairs.Where(x => x.Value <= 2).ToArray();
         Assert.Equal(filteredByLinq, filteredByJson);
     }
+
 }
